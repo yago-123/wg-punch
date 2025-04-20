e20ebec1ca55a3ffe4f025cc1e181956e19342ee
yago <me@yago.ninja>
2025-04-21 00:44:43 +0200

    feat(wg/userspace): add skeleton for userspace wg

--- pkg/wg/userspace/userspace-wg.go
+++ pkg/wg/userspace/userspace-wg.go
@@ -1,1 +1,23 @@
 package userspacewg
+
+import (
+	"context"
+	"github.com/yago-123/wg-punch/pkg/peer"
+	"github.com/yago-123/wg-punch/pkg/wg"
+	"net"
+)
+
+type userspaceWGTunnel struct {
+}
+
+func NewTunnel(cfg *wg.TunnelConfig) wg.Tunnel {
+	return &userspaceWGTunnel{}
+}
+
+func (uwgt *userspaceWGTunnel) Start(ctx context.Context, conn *net.UDPConn, localPrivKey string, peer peer.Info) error {
+	return nil
+}
+
+func (uwgt *userspaceWGTunnel) Stop() error {
+	return nil
+}

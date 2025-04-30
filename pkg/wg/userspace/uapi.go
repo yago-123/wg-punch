9a811912ea030ea46d3a9128e295d6305bdbcf62
yago <me@yago.ninja>
2025-04-30 16:08:43 +0200

    feat(wg/userspace): add UAPI conversion from wgtypes.Config into string

---
+++ pkg/wg/userspace/uapi.go
@@ -0,0 +1,49 @@
+package userspacewg
+
+import (
+	"encoding/hex"
+	"fmt"
+	"golang.zx2c4.com/wireguard/wgctrl/wgtypes"
+	"strings"
+)
+
+// ConvertWgTypesToUAPI converts WireGuard configuration to a UAPI string format
+func ConvertWgTypesToUAPI(cfg wgtypes.Config) (string, error) {
+	var b strings.Builder
+
+	if cfg.PrivateKey != nil {
+		b.WriteString(fmt.Sprintf("private_key=%s\n", hex.EncodeToString(cfg.PrivateKey[:])))
+	}
+
+	if cfg.ListenPort != nil {
+		b.WriteString(fmt.Sprintf("listen_port=%d\n", *cfg.ListenPort))
+	}
+
+	if cfg.ReplacePeers {
+		b.WriteString("replace_peers=true\n")
+	}
+
+	for _, peer := range cfg.Peers {
+		b.WriteString(fmt.Sprintf("public_key=%s\n", hex.EncodeToString(peer.PublicKey[:])))
+
+		if peer.Endpoint != nil {
+			b.WriteString(fmt.Sprintf("endpoint=%s\n", peer.Endpoint.String()))
+		}
+
+		if peer.ReplaceAllowedIPs {
+			b.WriteString("replace_allowed_ips=true\n")
+		}
+
+		for _, ipNet := range peer.AllowedIPs {
+			b.WriteString(fmt.Sprintf("allowed_ip=%s\n", ipNet.String()))
+		}
+
+		if peer.PersistentKeepaliveInterval != nil {
+			b.WriteString(fmt.Sprintf("persistent_keepalive_interval=%d\n", int(peer.PersistentKeepaliveInterval.Seconds())))
+		}
+
+		b.WriteString("\n") // Separate peers
+	}
+
+	return b.String(), nil
+}
